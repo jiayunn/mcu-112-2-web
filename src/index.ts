1988efe62b7af1bac1557e4dec5120b5549f660c
jiayun <jean93327@gmail.com>
2024-04-10 18:55:26 +0800

    feat(ts): 利用陣列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10)
-);
+const product = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+  new Product(
+    1,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+];
 console.log(product);
 console.table(product);
