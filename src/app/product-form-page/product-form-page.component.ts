ce5abd1e191a6948c20cb1cf65798286c597afba
jiayun <jean93327@gmail.com>
2024-06-06 15:15:55 +0800

    feat(product-form): 定義多筆作者輸入

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { Product } from '../model/product';
 import { map } from 'rxjs';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IProductForm } from '../interface/product-form.interface';
 
 @Component({
@@ -19,14 +19,24 @@ export class ProductFormPageComponent implements OnInit {
   form = new FormGroup<IProductForm>({
     id: new FormControl<number | null>(null),
     name: new FormControl<string | null>(null),
+    authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
     price: new FormControl<string | null>(null),
   });
 
+  get authors(): FormArray<FormControl<string | null>> {
+    return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
   product!: Product;
 
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthors(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
   }
 }
